e5c3c8cade1530f3d489888a11e81c5a4926ef31
Vladimir Svetlakov <vova.svetl@mail.ru>
2025-01-29 02:46:20 +0300

    feat: add restaurant markers to the map using API

--- src/app/(root)/components/map.tsx
+++ src/app/(root)/components/map.tsx
@@ -1,12 +1,16 @@
 'use client'
-import { useRef } from 'react';
-import { MapContainer, TileLayer, useMapEvent } from 'react-leaflet';
+import { fetchRestaurants, Restaurant } from '@/api/mosData';
+import { useRef, useState } from 'react';
+import { MapContainer, Marker, TileLayer, useMapEvent } from 'react-leaflet';
+import L from "leaflet";
 
-function SetViewOnClick({ animate }: { animate: boolean}) {
-  const map = useMapEvent('click', (e) => {
+function SetViewOnClick({ animate, setRestaurants }: { animate: boolean, setRestaurants: (restaurants: Restaurant[]) => void}) {
+  const map = useMapEvent('click', async (e) => {
     map.setView(e.latlng, map.getZoom(), {
       animate: animate,
     });
+    const response = await fetchRestaurants({OperatingCompany: 'Хлеб'});
+    setRestaurants(response);
   });
 
   return null;
@@ -14,6 +18,14 @@ function SetViewOnClick({ animate }: { animate: boolean}) {
 
 export function Map() {
   const mapRef = useRef(null);
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+  const icon = new L.Icon({
+    iconUrl: 'https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconSize: [22, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
 
   return (
         <MapContainer style={{height: "100%", width: "100%" }}
@@ -25,7 +37,17 @@ export function Map() {
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <SetViewOnClick animate={true} />
+
+          <ul>
+            {restaurants.map((item) => {
+              return (
+                <li key={item.global_id}>
+                  <Marker position={[item.geoData.coordinates[1], item.geoData.coordinates[0]]} icon={icon}/>
+                </li>
+              );
+            })}
+          </ul>
+          <SetViewOnClick animate={true} setRestaurants={setRestaurants}/>
         </MapContainer>
   );
 }
