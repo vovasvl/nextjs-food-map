c11064ee7512cfafe9b5a667d4433e3f72c00ea0
Vladimir Svetlakov <vova.svetl@mail.ru>
2025-01-29 03:39:14 +0300

    refactor: convert client API to server API

--- src/api/restaurants.ts
+++ src/api/restaurants.ts
@@ -1,13 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 const datasetId = 1903;
-const limit = 1000; 
+const limit = 1000;
 
 const api = axios.create({
   baseURL: `https://apidata.mos.ru/v1/datasets/${datasetId}/rows`,
   params: {
     api_key: process.env.API_KEY,
-  }
+  },
 });
 
 export interface Restaurant {
@@ -35,52 +36,52 @@ export interface Restaurant {
 }
 
 export interface RestaurantFilters {
-  OperatingCompany?: string,
-  TypeObject?: string,
-  IsNetObject?: boolean
+  OperatingCompany?: string;
+  TypeObject?: string;
+  IsNetObject?: boolean;
 }
 
-type FetchRestaurantsResponse  = Restaurant[];
-
 type ApiResponse = {
-  "global_id": number,
-  "Number": number,
-  "Cells": Restaurant
+  global_id: number;
+  Number: number;
+  Cells: Restaurant;
 }[];
 
-export async function fetchRestaurants(filters: RestaurantFilters): Promise<FetchRestaurantsResponse> {
+export default async function GET(req: NextApiRequest, res: NextApiResponse<Restaurant[] | { error: string }>) {
   try {
+    const filters: RestaurantFilters = req.query;
 
     const filterString = Object.entries(filters)
-    .filter(([, value]) => value !== undefined)
-    .map(([key, value]) => `Cells/${key} eq '${value}'`)
-    .join(' and ');
+      .filter(([, value]) => value !== undefined)
+      .map(([key, value]) => `Cells/${key} eq '${value}'`)
+      .join(' and ');
 
     const response = await api.get<ApiResponse>('', {
       params: {
-        $filter: filterString
-      }
+        $filter: filterString,
+      },
     });
 
     let allData = response.data;
     let dataCount = allData.length;
     let totalDataCount = dataCount;
 
-    while(dataCount === limit){
-      const newResponse = await api.get('', {
+    while (dataCount === limit) {
+      const newResponse = await api.get<ApiResponse>('', {
         params: {
           $skip: totalDataCount,
-          $filter: filterString
-        }
+          $filter: filterString,
+        },
       });
       dataCount = newResponse.data.length;
       totalDataCount += dataCount;
       allData = allData.concat(newResponse.data);
     }
 
-    return allData.map((item) => item.Cells);
+    const restaurants = allData.map((item) => item.Cells);
+    res.status(200).json(restaurants);
   } catch (error) {
     console.error('Ошибка при загрузке данных:', error);
-    throw error;
+    res.status(500).json({ error: 'Ошибка при загрузке данных' });
   }
-};
+}
